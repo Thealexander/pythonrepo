71aa07cff0316a682ac148833a69b3d873dece04
Brayan <Thealexander@users.noreply.github.com>
2016-08-31 17:13:05 -0600

    Update ejercicio.py
    creacion de codigo de clases

--- ejercicio.py
+++ ejercicio.py
@@ -1,1 +1,19 @@
+#! usr/bin/python
 
+#tarea de clases en python
+class Humano:
+    def INICIO (self,nom):
+        self.name=nom
+
+    def apantalla(self):
+        print 'Name:'
+        print self.name
+        print '<br>'
+
+persona1=Humano()
+persona1.INICIO('lOTERIA')
+persona1.apantalla()
+
+persona2=Humano()
+persona2.INICIO('rIGOBERTO')
+persona2.apantalla()
